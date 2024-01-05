bd325697a214f828f61ecd76bad922714d7114a2
Jestzer <James@Buonopane.email>
2024-01-05 13:25:37 -0500

    Can use the Enter key to close the error window more easily. Tab can't be used to open the GitHub URL from this window when an error is being displayed anymore.

--- ErrorWindow.xaml.cs
+++ ErrorWindow.xaml.cs
@@ -23,6 +23,13 @@ namespace Options.File.Checker.WPF
         {
             Close();
         }
+        private void CloseWithEnterKey(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Close();
+            }
+        }
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             // Open the URL in the default browser.
